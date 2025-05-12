367753a2c1b03a40d3f30d3bca1e0d609aa2eb0d
burak-ezer <burakezerrr@gmail.com>
2025-05-12 18:33:59 +0300

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,11 +1,7 @@
-let newArray = [];
-
 function makeArray(firstArray, secondArray, maxLength) {
-  newArray = firstArray.concat(secondArray);
+  const newArray = firstArray.concat(secondArray);
   if (newArray.length > maxLength) {
     newArray.length = maxLength;
-  }  
+  }
   return newArray;
 }
-
-console.log(newArray);
