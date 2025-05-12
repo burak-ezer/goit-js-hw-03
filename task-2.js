e52e4a3b94b276d23f6375731bcc4aa8a2b9f9e2
burak-ezer <burakezerrr@gmail.com>
2025-05-12 04:29:59 +0300

    Delete task-2.js

--- task-2.js
+++
@@ -1,7 +0,0 @@
-function makeArray(firstArray, secondArray, maxLength) {
-  const newArray = firstArray.concat(secondArray);
-  if (newArray.length > maxLength) {
-    newArray.length = maxLength;
-  }
-  return newArray;
-}
