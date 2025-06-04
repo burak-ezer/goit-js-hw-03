f0f6161c2d369d54ecde8f6f17553f0710bfba06
burak-ezer <burakezerrr@gmail.com>
2025-06-04 12:45:08 +0300

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -3,7 +3,6 @@ function makeArray(firstArray, secondArray, maxLength) {
   if (newArray.length > maxLength) {
     newArray.length = maxLength;
   }
-  
+  console.log(newArray);
   return newArray;
 }
-console.log(newArray);
